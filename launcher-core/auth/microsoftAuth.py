35e93caaef9d59e6e8310d053fa5e39355a76974
CubeSugarCheese <cubesugarcheese@qq.com>
2021-09-05 11:57:11 +0800

    添加基础的 zip 处理

--- launcher-core/auth/microsoftAuth.py
+++ launcher-core/auth/microsoftAuth.py
@@ -1,12 +1,15 @@
 import time
 import webview
 
+
 class MicrosoftAccount:
 
-
     def __init__(self):
-        window = webview.create_window("")
-        webview.start(print(window.get_current_url), window)
+        webview.create_window("https://login.live.com/oauth20_authorize.srf?client_id=00000000402b5328"
+                              "&response_type=code&scope=service%3A%3Auser.auth.xboxlive.com%3A%3AMBI_SSL"
+                              "&redirect_uri=https%3A%2F%2Flogin.live.com%2Foauth20_desktop.srf")
+        webview.start()
+
 
 if __name__ == '__main__':
     test = MicrosoftAccount()
