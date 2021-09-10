006063197c2c9cb442a0e17460e4286543505ff8
CubeSugarCheese <cubesugarcheese@qq.com>
2021-09-10 10:32:02 +0800

    添加参数

---
+++ launcherCore/launcher.py
@@ -0,0 +1,28 @@
+
+
+class Launcher:
+    pass
+class GameInfo:
+    minecraft_path: str
+    # D:/mc/.minecraft
+    RAM_max: str
+    # 4096
+    name: str
+    # Cubesugarcheese
+    uuid: str
+    # 55eff3377c554216844686450b4b3fef
+    mc_access_token:str
+    #
+    game_dir: str
+    #
+    version: str
+
+
+    # "{\"preferredLanguage\":[\"zh-cn\"],\"registrationCountry\":[\"CN\"]}"
+
+    java_path: str
+
+    authlib_path: str
+
+    assets_path: str
+
