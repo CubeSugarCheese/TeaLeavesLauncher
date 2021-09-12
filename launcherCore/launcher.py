0f17a608da935354daf769aefcdc26cf00b17859
0x7FFFFF <w13657804853@163.com>
2021-09-12 21:07:15 +0800

    问题修正完毕，现在可以启动原版游戏了

--- launcherCore/launcher.py
+++ launcherCore/launcher.py
@@ -1,5 +1,4 @@
 import json
-import os
 import platform
 from utils.javaFinder import find_java_from_where
 
@@ -38,7 +37,7 @@ class Launcher:
         self.width = width
         self.height = height
         self.max_memory = max_memory
-        if mc_access_token:
+        if mc_access_token is not None:
             self.user_type = "Mojang"
             self.mc_access_token = mc_access_token
         else:
@@ -52,7 +51,7 @@ class Launcher:
             return json.loads(f.read())
 
     def _generate_libraries_parameter(self):
-        jar_paths = "\""
+        jar_paths = ""
         for i in self._load_version_json()["libraries"]:
             if "classifiers" not in i["downloads"]:
                 libraries_parts = i["name"].split(":")
@@ -63,25 +62,24 @@ class Launcher:
                     jar_paths += f"{library_path};"
                 else:
                     jar_paths += f"{library_path}:"
-        jar_paths += "\""
         return jar_paths
 
     def _generate_launch_parameter(self):
         part_X = f"java -Xmx{self.max_memory}M -XX:+UseG1GC -XX:-UseAdaptiveSizePolicy -XX:-OmitStackTraceInFastThrow "
-        part_D = f"-Dminecraft.client.jar=\".minecraft\\versions\\{self.version}\\{self.version}.jar \" -Dminecraft.launcher.brand=tea-leaves-launcher -Dminecraft.launcher.version={self.launcher_version} -Djava.library.path={self.natives_folder_path} "
-        part_cp = f"-cp {self._generate_libraries_parameter()} "
-        part_jar = f"\"{self.mc_path}\\versions\\{self.version}\\{self.version}.jar\" net.minecraft.client.main.Main "
+        part_D = f"-Dminecraft.launcher.brand=tea-leaves-launcher -Dminecraft.launcher.version={self.launcher_version} -Djava.library.path={self.natives_folder_path} "
+        part_cp = f"-cp \"{self._generate_libraries_parameter()}{self.mc_path}\\versions\\{self.version}\\{self.version}.jar\" "
+        part_class = f"net.minecraft.client.main.Main "
         if self.user_type == "Legacy":
             part_game = f"--username {self.name} --version {self.version} --gameDir \"{self.mc_path}\\versions\\{self.version}\" " + \
                         f"--assetsDir \"{self.mc_path}\\assets\" --assetIndex {self.asset_index} " + \
-                        f"--uuid {self.uuid} --userType {self.user_type} --versionType TeaLeavesLauncher" + \
+                        f"--userType mojang --uuid {self.uuid} --accessToken f266a689f9b1424995bc341fc0966560 --versionType TeaLeavesLauncher " + \
                         f"--width {self.width} --height {self.height}"
         else:
             part_game = f"--username {self.name} --version {self.version} --gameDir \"{self.mc_path}\\versions\\{self.version}\" " + \
                         f"--assetsDir \"{self.mc_path}\\assets\" --assetIndex {self.asset_index} " + \
-                        f"--uuid {self.uuid} --accessToken {self.mc_access_token} --versionType TeaLeavesLauncher " + \
+                        f"--userType mojang --uuid {self.uuid} --accessToken {self.mc_access_token} --versionType TeaLeavesLauncher " + \
                         f"--width {self.width} --height {self.height}"
-        return part_X + part_D + part_cp + part_jar + part_game
+        return part_X + part_D + part_cp + part_class + part_game
 
     def launch_game(self):
         cmd = self._generate_launch_parameter()
@@ -94,7 +92,7 @@ if __name__ == "__main__":
         mc_path=r"C:\Users\2026\Downloads\Programs\bakaxl\.minecraft",
         name="Cubesugarcheese",
         uuid="55eff3377c554216844686450b4b3fef",
-        mc_access_token="",
+        #        mc_access_token="",
         version="1.16.5"
     )
     print(testLauncher._generate_launch_parameter())
