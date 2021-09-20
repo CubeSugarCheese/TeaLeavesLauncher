0883000d1845c681c30aae937b894ddcc62e7047
CubeSugarCheese <cubesugarcheese@qq.com>
2021-09-20 17:02:41 +0800

    添加外置登录支持

--- launcherCore/auth/authlibInjectorAuth.py
+++ launcherCore/auth/authlibInjectorAuth.py
@@ -1,5 +1,6 @@
-import aiohttp
-import asyncio
+import requests
+import json
+import logging
 
 
 class AuthlibInjectorAccount:
@@ -7,30 +8,43 @@ class AuthlibInjectorAccount:
     username: str  # 此处为邮箱或以外的账户标识
     password: str
     uuid: str
+    mc_access_token: str
 
     def __init__(self,
                  api_address,
-                 username,
-                 password,
+                 username=None,
+                 password=None,
+                 mc_access_token=None,
+                 uuid=None,
                  client_token: str = None):
         self.client_token = client_token
         self.username = username
         self.password = password
         self.api_address = api_address
-        self.api_address = asyncio.get_event_loop().run_until_complete(self._get_reality_api_address())
-        self.auth_data = asyncio.get_event_loop().run_until_complete(self._get_authenticate())
+        self.api_address = self._get_reality_api_address()
+        if mc_access_token is not None:
+            self.mc_access_token = mc_access_token
+            self.mc_access_token = self._get_mc_access_token()
+            self.username = username
+            self.uuid = uuid
+        else:
+            self.auth_data = self._get_authenticate().json()
+            self.mc_access_token = self._get_mc_access_token()
+            self.available_profiles = self.auth_data["availableProfiles"]
+            self.profile = self._select_profile()
+            self.username = self.profile["name"]  # 此处将 username 从登录凭据转换为角色名用以生成启动参数
+            self.uuid = self.profile["id"]
 
-    async def _get_reality_api_address(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(self.api_address) as response:
-                headers = response.headers
-                if "x-authlib-injector-api-location" in headers:
-                    api_address = headers["x-authlib-injector-api-location"]
-                else:
-                    api_address = self.api_address
-                return api_address
+    def _get_reality_api_address(self):
+        response = requests.get(self.api_address)
+        headers = response.headers
+        if "x-authlib-injector-api-location" in headers:
+            api_address = headers["x-authlib-injector-api-location"]
+        else:
+            api_address = self.api_address
+        return api_address
 
-    async def _get_authenticate(self):
+    def _get_authenticate(self):
         address = f"{self.api_address}/authserver/authenticate"
         headers = {"Content-Type": "application/json"}
         if self.client_token is not None:
@@ -42,7 +56,58 @@ class AuthlibInjectorAccount:
             data = dict(username=self.username,
                         password=self.password,
                         agent={"name": "Minecraft", "version": 1})
-        async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.post(address, data=data) as response:
-                t = await response.json()
-                return t
+        response = requests.post(address, data=json.dumps(data), headers=headers)
+        return response
+
+    def _refresh_access_token(self):
+        address = f"{self.api_address}/authserver/refresh"
+        headers = {"Content-Type": "application/json"}
+        if self.client_token is not None:
+            data = dict(accessToken=self.mc_access_token,
+                        clientToken=self.client_token)
+        else:
+            data = dict(accessToken=self.mc_access_token)
+        response = requests.post(address, data=data, headers=headers)
+        return response
+
+    def _check_access_token_is_available(self):
+        address = f"{self.api_address}/authserver/validate"
+        headers = {"Content-Type": "application/json"}
+        if self.client_token is not None:
+            data = dict(accessToken=self.mc_access_token,
+                        clientToken=self.client_token)
+        else:
+            data = dict(accessToken=self.mc_access_token)
+        response = requests.post(address, data=data, headers=headers)
+        if response.status_code == 204:
+            result = True
+        else:
+            result = False
+        return result
+
+    def _select_profile(self):
+        if len(self.available_profiles) == 1:
+            selected_profile = self.available_profiles[0]
+        else:
+            print("检测到有多个可用的角色，请选择一个登录：")
+            for i in self.available_profiles:
+                print(f"【{self.available_profiles.index(i)}】{i['name']}")
+            selected_profile = self.available_profiles[(input("选择："))]
+        return selected_profile
+
+    def _get_mc_access_token(self):
+        if self.username is not None and self.password is not None:
+            token = self.auth_data["accessToken"]
+        elif self.mc_access_token is not None:
+            if self._check_access_token_is_available():
+                token = self.mc_access_token
+            else:
+                response = self._refresh_access_token()
+                if response.status_code == 200:
+                    token = response.json()["accessToken"]
+                else:
+                    logging.warning("登录凭据已失效，需要重新登录")
+                    self.username = input("账户：")
+                    self.password = input("密码：")
+                    token = self._get_authenticate().json()["accessToken"]
+        return token
