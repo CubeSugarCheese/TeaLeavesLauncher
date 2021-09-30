5f1d9775870ec85cd057adf4e955fc89c89be2c6
CubeSugarCheese <cubesugarcheese@qq.com>
2021-09-30 22:09:54 +0800

    更新默认设置的说明

--- launcherCore/utils/static.py
+++ launcherCore/utils/static.py
@@ -1,9 +1,14 @@
 default_config = r"""
 clientToken:
+# 用于登录验证
 auto_generate_clientToken: true
+# 在 clientToken 为空时自动生成一个随机的值
 max_memory: 4096
+# 全局设置的最大内存使用
 java_path:
 - C:\example\Java\bin\java.exe
+# java.exe 的完整路径
 MC_paths:
 - C:\example\.minecraft
+# .minecraft 文件夹的完整路径
 """
